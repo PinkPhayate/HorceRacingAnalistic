98268b5a57e87b3a7c2ccefc96a8f8cde87d0368
Tanaka Hayate <pinqphayate@gmail.com>
2016-08-13 11:42:07 +0900

    modify sgd module anable to build ierately

--- Source/sgd.py
+++ Source/sgd.py
@@ -3,14 +3,10 @@ import pandas as pd
 import csv
 from sklearn.linear_model import SGDClassifier
 ALL_PARAMS = ['frame', 'num','age','odds','fav','wght','qntty','f','m','z','p','m']
+ITERATION = 10
+THRESHOLD = 0.5
 
-def predict_via_sgd(years):
-    dfs = de.create_merged_df(years)
-    f = open('./../Result/sgd_default.csv', 'ab')
-    csvWriter = csv.writer(f)
-
-    for race_id in years:
-        print race_id
+def predict_via_sgd(dfs, race_id):
         evalt_df = dfs[dfs['race_id'] == race_id]
         train_df = dfs[dfs['race_id'] != race_id]
 
@@ -25,10 +21,7 @@ def predict_via_sgd(years):
 
         predicts = clf.predict(eX)
         print predicts
-        list = [race_id, ]
-        list.append(predicts.tolist())
-        csvWriter.writerow(list)
-    f.close
+        return predicts.tolist()
 
 
 if __name__ == '__main__':
@@ -39,4 +32,26 @@ if __name__ == '__main__':
     '''
     df = pd.read_csv('./../Data/race_info.csv', header=None)
     years = df[9]
-    predict_via_sgd( years )
+    # predict_via_sgd( years )
+    dfs = de.create_merged_df(years)
+    f = open('./../Result/sgd_default.csv', 'ab')
+    csvWriter = csv.writer(f)
+
+    for race_id in years:
+        print race_id
+        # predict iteratly
+        sum_list = predict_via_sgd(dfs,race_id)
+        for i in range(0, ITERATION-1):
+            list = predict_via_sgd(dfs,race_id)
+            sum_list = [x+y for (x, y) in zip(sum_list, list)]
+        # circulate average
+        list = map(lambda x: float(x) / ITERATION, sum_list)
+
+        # call for pay list
+        pay_list = [race_id,]
+        for index in range(1,len(list)):
+            if list[index] >= THRESHOLD:
+                pay_list.append(index)
+        print pay_list
+        csvWriter.writerow(pay_list)
+    f.close
